20094cb315b197433d0581ce0d75893d908177ef
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2025-12-28 13:28:56 +0100

    aggiunta funzione per il calcolo del prezzo del biglietto

--- js/script.js
+++ js/script.js
@@ -6,5 +6,19 @@ const formElement = document.querySelector("form");
 
 formElement.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log("evento");
+    const totale = ticketPrice(numberKmInputElement.value, numberAgeElement.value)
+    console.log(totale);
 })
+
+function ticketPrice (km, age){
+    const kmPrice = 0.21;
+    const underageDiscount = 0.20;
+    const over65discount = 0.40;
+    if (age >= 0 && age < 18) {
+        return km * kmPrice * (1 - underageDiscount);
+    } else if (age >= 18 && age < 65) {
+        return km * kmPrice;
+    } else {
+        return km * kmPrice * (1 - over65discount);
+    }
+}
