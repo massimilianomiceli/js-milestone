f653f11dbca94c3daab8dfdd89a066e1d15d56f9
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2025-12-28 14:59:29 +0100

    Inserito campo nome e sistemato un po lo stile esercizio terminato

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
+const passengerNameInputElement = document.getElementById("passengerName");
 const numberKmInputElement = document.getElementById("numberKm");
 const numberAgeElement = document.getElementById("numberAge");
 const btnTotalElement = document.getElementById("btnTotal");
 const formElement = document.querySelector("form");
 const tbodyElement = document.querySelector("tbody");
-const tableTotal = document.getElementById("tableTotal");
+const tableTotalElement = document.getElementById("tableTotal");
+const summaryCardElement = document.getElementById("summary");
 var total = 0;
 
 formElement.addEventListener("submit", function(event){
     event.preventDefault();
 
+    summaryCardElement.classList.remove("d-none");
+
+
+    const newTdPassengerNameElement = document.createElement("td");
+    newTdPassengerNameElement.textContent = passengerNameInputElement.value;
     const newTdKmElement = document.createElement("td");
     newTdKmElement.textContent = numberKmInputElement.value;
     const newTdAgeElement = document.createElement("td");
     newTdAgeElement.textContent = numberAgeElement.value;
     const newTdTotalElement = document.createElement("td");
-    newTdTotalElement.textContent = ticketPrice(numberKmInputElement.value, numberAgeElement.value).toFixed(2);
+    newTdTotalElement.textContent = `${ticketPrice(numberKmInputElement.value, numberAgeElement.value).toFixed(2)} €`;
 
     const newTrElement = document.createElement("tr");
-    newTrElement.append(newTdKmElement, newTdAgeElement, newTdTotalElement);
+    newTrElement.append(newTdPassengerNameElement, newTdKmElement, newTdAgeElement, newTdTotalElement);
 
     tbodyElement.append(newTrElement);
     
     total += ticketPrice(parseFloat(numberKmInputElement.value), parseFloat(numberAgeElement.value));
 
-    tableTotal.textContent = (total).toFixed(2);
+    tableTotalElement.textContent = `${(total).toFixed(2)} €`;
 })
 
 function ticketPrice (km, age){
